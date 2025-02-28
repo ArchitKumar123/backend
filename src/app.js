e384ae7058b3cd00aff4a3faf6aad187922d5899
ArchitKumar123 <architraul23@gmail.com>
2025-03-01 02:32:41 +0530

    add getWatchHistory endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+//routes import
+import userRouter from "./routes/user.route.js"
 
 const app = express()
 
@@ -14,9 +16,6 @@ app.use(express.urlencoded({limit: "16kb", extended: true}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
-
-//routes import
-import userRouter from "./routes/user.route.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
